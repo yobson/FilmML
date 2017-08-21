1b6fb867d680094b0fa5a899c5c37ab8ddfcbdc1
jameshobson <jameshobson@DESKTOP-JNHBVF8>
2017-08-21 14:11:23 +0100

    Added CS Library

---
+++ CSLibrary/FilmML/FilmML.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace FilmML
+{
+    public class DLL
+    {
+        public const string DllName = "libfilmML.dll";
+        [DllImport(DllName)]
+        public static extern int initFilmML();
+        [DllImport(DllName)]
+        public static extern int addFilm(string filmName, int defaultFilmType);
+        [DllImport(DllName)]
+        public static extern int addUser();
+        [DllImport(DllName)]
+        public static extern void cleanUpUsers();
+        [DllImport(DllName)]
+        public static extern int dllTest();
+
+    }
+
+    public class FilmML
+    {
+    }
+}
