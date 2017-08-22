6b4284e83cc432d687d61e80242e1e9d571ea9e7
jameshobson <jameshobson@DESKTOP-JNHBVF8>
2017-08-22 11:12:40 +0100

    Added elastic search files

---
+++ Classes/ElasticSearch.m
@@ -0,0 +1,5 @@
+#import "../Headers/ElasticSearch.h"
+
+@implementation ElasticSearch
+
+@end
---
+++ Headers/ElasticSearch.h
@@ -0,0 +1,11 @@
+#import <ObjFW/OFObject.h>
+#import <ObjFW/OFString>
+#import <ObjFW/OFURL.h>
+#import <ObjFW/OFHTTPClient.h>
+#import <ObjFW/OFHTTPRequest.h>
+
+@interface ElasticSearch : OFObject {
+
+}
+
+@end;
