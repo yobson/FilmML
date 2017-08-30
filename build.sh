56d5bfeb15f1fdd601750a58ba8b1a75c3c7b341
jameshobson <jameshobson@DESKTOP-JNHBVF8>
2017-08-30 16:50:44 +0100

    Added clean script

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 objfw-compile -Ofast --lib 0.1 -o filmML main.m C/*.m Classes/*.m
 mv *.dll bin/.
-# rm -r *.o *.a
-# cd Classes
-# rm -r *.o
-# cd ../C
-# rm -r *.o
---
+++ clean.sh
@@ -0,0 +1,7 @@
+rm -r *.o *.a
+cd Classes
+rm -r *.o
+cd ../C
+rm -r *.o
+cd ../bin
+rm libfilmML.dll
